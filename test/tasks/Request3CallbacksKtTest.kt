df292173812148edcbdd4655b9d3f5fa45203052
inoy <7m7110123@gmail.com>
2025-02-09 17:48:08 +0900

    tips: Pass assertEquals as a method parameter

--- test/tasks/Request3CallbacksKtTest.kt
+++ test/tasks/Request3CallbacksKtTest.kt
@@ -10,10 +10,15 @@ class Request3CallbacksKtTest {
     @Test
     fun testDataIsLoaded() {
         loadContributorsCallbacks(MockGithubService, testRequestData) {
+            // このassertEqualsの仕方おもしろい
             Assert.assertEquals(
                 "Wrong result for 'loadContributorsCallbacks'",
                 expectedResults.users, it
             )
+            // 第3引数はupdateResults関数を受け取ることになっているところにassertEqualsを渡してる
+            // updateResultsは引数にloadしたContributorsを引数に受け取る作りになっている
+            // そのためitが指定された引数になってるっぽい？(なんでやちゃんと理解できてない)
+            // なのでitで期待値/Contributorsをここで指定できてる
         }
     }
 }
