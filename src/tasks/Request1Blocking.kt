84c6b5b443329d8330e55a746744df5540fbf619
inoy <7m7110123@gmail.com>
2025-02-09 17:14:19 +0900

    Add comments explaining the blocking HTTP request process
    https://kotlinlang.org/docs/coroutines-and-channels.html#blocking-requests

--- src/tasks/Request1Blocking.kt
+++ src/tasks/Request1Blocking.kt
@@ -3,12 +3,16 @@ package tasks
 import contributors.*
 import retrofit2.Response
 
-fun loadContributorsBlocking(service: GitHubService, req: RequestData) : List<User> {
+fun loadContributorsBlocking(service: GitHubService, req: RequestData): List<User> {
     val repos = service
+        // return an instance of the *Call class (#1). At this point, no request is sent.
         .getOrgReposCall(req.org)
+        // *Call.execute() is then invoked to perform the request (#2). execute() is a synchronous call that blocks the underlying thread.
         .execute() // Executes request and blocks the current thread
+        // When you get the response, the result is logged by calling the specific logRepos() and logUsers() functions (#3). If the HTTP response contains an error, this error will be logged here.
         .also { logRepos(req, it) }
-        .body() ?: emptyList()
+        // Finally, get the response's body, which contains the data you need. For this tutorial, you'll use an empty list as a result in case there is an error, and you'll log the corresponding error (#4).
+        .bodyList()
 
     return repos.flatMap { repo ->
         service
