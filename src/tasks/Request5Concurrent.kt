1982f35458303c7f85d0c453f811e0614d38d53b
inoy <7m7110123@gmail.com>
2025-02-11 18:47:44 +0900

    Task5: add concurrent loading of contributors using coroutines

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,8 +1,26 @@
 package tasks
 
-import contributors.*
-import kotlinx.coroutines.*
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.logRepos
+import contributors.logUsers
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service.getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferred: List<Deferred<List<User>>> = repos.map { repo ->
+        async {
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    deferred.awaitAll().flatten().aggregate()
 }
